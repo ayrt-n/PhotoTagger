d8b99cb9a53d9a98cf3d01c3b56f7f56562c406b
ayrton <ayrton.parkinson1@gmail.com>
2024-07-31 08:27:54 -0400

    Initial Commit

---
+++ Day77Challenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Day77Challenge
+//
+//  Created by Ayrton Parkinson on 2024/07/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day77Challenge/Day77ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day77ChallengeApp.swift
+//  Day77Challenge
+//
+//  Created by Ayrton Parkinson on 2024/07/31.
+//
+
+import SwiftUI
+
+@main
+struct Day77ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
